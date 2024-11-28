2e163cd63ed8f1cc9c7557c0f6cd128e1cabf675
Shoaib Hasan Siam <111237026+Shoaib-Siam@users.noreply.github.com>
2024-11-28 21:33:51 +0600

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Auth\Access\Gate;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::paginate(5);
+
+        return view('posts', [
+            'posts' => $posts
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|unique:posts|max:255',
+            'contents' => 'required'
+        ]);
+
+        // Post::create([
+        //     'title' => $validated['title'],
+        //     'contents' => $validated['contents'],
+        //     'user_id' => auth()->user()->id
+        // ]);
+
+        $user = auth()->user();
+
+        $user->posts()->create($validated);
+
+        return redirect('/posts');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        return view('post', [
+            'post' => $post
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post)
+    {
+        return view ('edit', [
+            'post' => $post
+            ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        // Gate::authorize('update-post', $post);
+        $validated = $request->validate([
+            'title' => 'required|unique:posts|max:255',
+            'contents' => 'required'
+        ]);
+
+       
+     
+       
+
+        $post->update($validated);
+
+        return redirect('/posts');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        abort_if($post->author->id !== auth()->user()->id, 401);
+
+        $post->delete();
+
+        return redirect('/posts');
+    }
+}
---
+++ resources/views/edit.blade.php
@@ -0,0 +1,50 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Edit Post') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <form action="/posts/{{$post->id}}" method="post" class="space-y-6">
+                        @method('patch')
+                        @csrf
+                        <div class="flex flex-col space-y-2">
+                            <label for="title" class="text-sm font-medium text-gray-700">Title</label>
+                            <input
+                                type="text"
+                                name="title"
+                                id="title"
+                                value="{{ $post->title }}"
+                                class="border border-gray-300 bg-white rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                            >
+                            @error('title')
+                                <p class="text-sm text-red-600">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <div class="flex flex-col space-y-2">
+                            <label for="contents" class="text-sm font-medium text-gray-700">Contents</label>
+                            <textarea
+                                name="contents"
+                                id="contents"
+                                class="border border-gray-300 bg-white rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 h-32 resize-none"
+                            >{{ $post->contents }}</textarea>
+                            @error('body')
+                                <p class="text-sm text-red-600">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <button
+                            type="submit"
+                            class="inline-flex items-center px-4 py-2 bg-blue-500 text-white rounded-md font-semibold text-xs uppercase tracking-widest shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition ease-in-out duration-150"
+                        >
+                            Update Post
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
